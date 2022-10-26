32e524f5d1be6370c78f66d2519048469cf7a649
Milly <millylee@qq.com>
2022-10-26 14:02:33 +0800

    fix: error fixed

--- index.js
+++ index.js
@@ -1,58 +1,47 @@
+require('dotenv').config()
 const env = process.env || {};
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  // 标签页变化
-  browser.on('targetchanged', async target => {
-    const newPage = await target.page();
-    // url变化
-    newPage.on('framenavigated', async () => {
-      if (newPage.url().indexOf('/user') && browser.isConnected) {
-        await newPage.waitForSelector('.breadcrumb-item > .btn');
-        // TODO: newPage.click() 适配方法
-        await newPage.evaluate(async () => {
-          const selector = document.querySelector('.breadcrumb-item > .btn');
-          if (selector) {
-            if (selector.textContent.trim() === '明日再来') {
-              console.log('[Sign] %s', '今日已签到');
-            } else {
-              document.querySelector('.breadcrumb-item > .btn').click();
-              console.log('[Sign] %s', '签到完成');
-            }
+  console.time('AutoSign');
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://www.hjtnt.link/auth/login");
+  await page.type('#email', env?.EMAIL || '');
+  await page.type('#password', env?.PASSWORD || '');
+  await page.click('.login');
+  const targets = await browser.targets();
+  targets.map(async target => {
+    const pageUrl = await target.url();
+    if (pageUrl.indexOf('oauth.telegram.org/embed/') > -1) {
+      const newBrowser = await target.browser();
+      const newPage = await newBrowser.newPage();
+      await newPage.goto('https://www.hjtnt.link/user');
+      await newPage.waitForSelector('.breadcrumb-item > .btn');
+      // TODO：newPage.click() 不能执行
+      const ret = await newPage.evaluate(() => {
+        const selector = document.querySelector('.breadcrumb-item > .btn');
+        if (selector) {
+          if (selector.textContent.trim() === '明日再来') {
+            return Promise.resolve('今日已签到');
+            
           } else {
-            console.log('[Sign] %s', '签到元素节点不存在');
+            document.querySelector('.breadcrumb-item > .btn').click();
+            return Promise.resolve('签到完成');
           }
-        });
-        await browser.close();
-      }
-    });
-  });
-  try {
-    if( browser.isConnected) {
-      console.time('OpenLoginPage');
-      const page = await browser.newPage();
-      await page.goto('https://www.hjtnt.link/auth/login');
-      console.timeEnd('OpenLoginPage');
-
-      await page.type('#email', env?.EMAIL || '');
-      await page.type('#password', env?.PASSWORD || '');
-
-      const [response] = await Promise.all([
-        page.waitForNavigation(),
-        page.click('.login'),
-      ]);
-      await response;
-      console.log('填写邮箱与密码并进行登录');
-    }
-  } catch (error) {
-    console.log('签到出错', error.message);
-    if (browser.isConnected) {
+        } else {
+          return Promise.resolve('签到元素节点不存在');
+        }
+      });
+      console.log('[Sign] %s', ret);
       await browser.close();
+      console.timeEnd('AutoSign');
     }
-  }
+  });
 })();
 
 process.on('exit', (code) => {
-  console.log('异常退出：' + code)
+  if (code !== 0) {
+    console.log('异常退出：' + code)
+  }
 });
