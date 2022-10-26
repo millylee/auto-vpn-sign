f8e993c09a76ac87b1076042db775404b1976bf5
millylee <millylee@qq.com>
2022-10-27 01:20:50 +0800

    fix: open error

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ const env = process.env || {};
 const puppeteer = require('puppeteer');
 
 (async () => {
-  console.time('AutoSign');
-  const browser = await puppeteer.launch();
+  console.time('AutoSign'); 
+  const browser = await puppeteer.launch({
+    // args: ['--no-sandbox'],
+    // headless: env.NODE_ENV === 'development' ? false : true,
+    headless: false,
+  });
   const page = await browser.newPage();
+
+  console.log('[Sign] %s', '准备登录')
   await page.goto("https://www.hjtnt.link/auth/login");
   await page.type('#email', env?.EMAIL || '');
   await page.type('#password', env?.PASSWORD || '');
   await page.click('.login');
-  console.log('[Sign] %s', '登录成功')
+
+  console.log('[Sign] %s', '登录成功');
+
   const targets = await browser.targets();
   targets.map(async target => {
     const pageUrl = await target.url();
+    console.log('[Sign] %s', pageUrl)
     if (pageUrl.indexOf('oauth.telegram.org/embed/') > -1) {
       const newBrowser = await target.browser();
       const newPage = await newBrowser.newPage();
@@ -40,3 +49,7 @@ const puppeteer = require('puppeteer');
     }
   });
 })();
+
+process.on('uncaughtException', (err) => {
+  console.log('[Sign] %s - %s', '捕获未知错误', err.message);
+});
