d74ea209389bde194191757564f404ea60a21af9
millylee <millylee@qq.com>
2024-01-31 16:17:37 +0800

    chore: 调整为 python

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# 火箭 TNT
+# 自动签到
 
 > 每天自动领取流量
 
-Fork 仓库，在 `Settings - Secrets - Actions` 中添加 `EMAIL`、`PASSWORD`、`SITE_URL` 环境变量。然后切到 Actions 手动运行任务看下输出日志，成功后即在每小时执行一次，避免服务器异常导致领取失败。
+Fork 仓库，在 `Settings - Secrets - Actions` 中添加 `EMAIL`、`PASSWORD`、`SITE_URL`、`PUSHPLUS_TOKEN` 环境变量。然后切到 Actions 手动运行任务看下输出日志，成功后即在每小时执行一次，避免服务器异常导致领取失败。
 
 当前可用的域名后缀为 `.me`、`.top`
+
+改用 `python` 直接请求接口，原无头模式放 `nodejs-headless` 分支。
---
+++ main.py
@@ -0,0 +1,50 @@
+import requests, json, re, os
+
+session = requests.session()
+siteUrl = os.environ.get('SITE_URL')
+email = os.environ.get('EMAIL')
+passwd = os.environ.get('PASSWORD')
+pushPlusToken = os.environ.get('PUSHPLUS_TOKEN')
+
+loginUrl = '{}/auth/login'.format(siteUrl)
+checkInUrl = '{}/user/checkin'.format(siteUrl)
+pushPlusUrl = 'http://www.pushplus.plus/send'
+
+pushPlusData = {
+  'token': pushPlusToken,
+  'title': '签到领流量',
+  'template': 'txt',
+  'channel': 'wechat'
+}
+
+header = {
+  'origin': siteUrl,
+  'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+}
+
+data = {
+  'email': email,
+  'passwd': passwd
+}
+
+try:
+  print('[CheckIn] {} {}'.format('准备登录'))
+
+  response = json.loads(session.post(url=loginUrl,headers=header,data=data).text)
+  print('[CheckIn] {} {}'.format('请求数据', response['msg']))
+
+  result = json.loads(session.post(url=checkInUrl,headers=header).text)
+  content = result['msg']
+  print('[CheckIn] {} {}'.format('签到结果', content))
+
+  if pushPlusToken != '':
+    postData = json.loads(pushPlusData)
+    postData['content'] = content
+    requests.post(url=pushPlusUrl, data=json.dumps(postData))
+    print('[CheckIn] {}'.format('推送成功'))
+except Exception as e:
+  print('[CheckIn] {} {}'.format('签到失败', e))
+  if pushPlusToken != '':
+    postData = json.loads(pushPlusData)
+    postData['content'] = e
+    requests.post(url=pushPlusUrl, data=json.dumps(postData))
